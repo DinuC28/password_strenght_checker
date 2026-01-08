948f5f1813ab4ba490d2f1496523e23c05b5d021
Dinu Ciobanu <dinuciobanu28837@gmail.com>
2026-01-08 17:59:37 +0000

    repeating characters

--- password_checkerV2.py
+++ password_checkerV2.py
@@ -8,20 +8,39 @@ def password_length(password):
         number = False
 
         for char in password:
-            if(char.islower()):
-                lowerCase = True
-            if(char.isupper()):
-                upperCase = True
-            if(char.isdigit()):
-                number = True
-            if(not char.islnum()):
-                character = True
+            if char.islower():
+                return True
+            if char.isupper():
+                return True
+            if char.isdigit():
+                return True
+            if not char.islnum():
+                return True
 
         return False
     else:
         return True
 
+def repeating_character(password, maxRepeat):
+
+    character_count = 0
+    last_character = None
+
+    for i in range(1, len(password)):
+        last_character = password[i - 1]
+
+        if password[i] == last_character:
+            character_count += 1
+        else:
+            character_count = 0
+
+        if character_count == maxRepeat:
+            return False
+
+    return True
+
 
 UserInput = "Dinu123!"
 
 print(password_length(UserInput))
+print(repeating_character(UserInput, 2))
