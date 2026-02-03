206e2cce02ca2f5811500435996946a8002f5b6c
Dinu Ciobanu <dinuciobanu28837@gmail.com>
2026-02-03 19:34:19 +0000

    Basic html and css implementation with Flask

--- Flask_WebApp/app.py
+++ Flask_WebApp/app.py
@@ -1,8 +1,15 @@
-from flask import  Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+    #send the password to the python file
+    if request.method == "POST":
+        current_password = request.form.get("password")
+        
     return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
