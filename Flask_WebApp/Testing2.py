9bb0f56864af00043ccf7aa1a666f711289f2d43
Dinu Ciobanu <dinuciobanu28837@gmail.com>
2026-03-29 14:33:19 +0100

    Bug fixes

--- Flask_WebApp/Testing2.py
+++ Flask_WebApp/Testing2.py
@@ -5,17 +5,19 @@ from strength_checker import password_validation, password_strength
 def sample_test(sample = 1000):
     result = []
 
-    with open('rockyou.txt', encoding = 'utf-8') as file:
+    with open('rockyou.txt', encoding = 'latin-1') as file:
         password_sample = [next(file).strip() for i in range(sample)]
 
     for password in password_sample:
-        score = password_strength(password)
-        validation = password_validation(password)
+        score, messages  = password_strength(password)
+        validation, feedback_messages = password_validation(password)
         zxcvbn_score = zxcvbn(password)
 
         result.append({
             'my_score': score,
+            'my_messages': messages,
             'my_validation': validation,
+            'my_feedback_messages': feedback_messages,
             'zxcvbn_score': zxcvbn_score['score'],
             'zxcvbn_validation': zxcvbn_score['feedback']
         })
@@ -56,13 +58,13 @@ def results_testing(result):
             disagreements += 1
 
     total = len(result)
-    print(f"Agreement rate: {agreements}/{total}" ({(agreements / total) * 100}))
-    print(f"Disagreement rate: {disagreements}/{total}" ({(disagreements / total) * 100}))
+    print(f"Agreement rate: {agreements}/{total}", ({(agreements / total) * 100}),"%")
+    print(f"Disagreement rate: {disagreements}/{total}", ({(disagreements / total) * 100}),"%")
 
 if __name__ == '__main__':
     print("== RockYou testing ==")
 
     test = sample_test(1000)
     results_testing(test)
-    print("== RockYou testing ==")
+
 
